9d2c0e24e64d80559b7b5cc94ec5f1663dfdefa0
Athiban <93418753+Athiban-32@users.noreply.github.com>
2022-10-10 15:33:50 +0530

    Update FooterBanner.jsx

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -24,8 +24,6 @@ const FooterBanner = ({footerBanner: {
                 </Link>
 
             </div>
-            <img src={urlFor(image)}
-            className='footer-banner-image'/>
         </div>
 
     </div>
